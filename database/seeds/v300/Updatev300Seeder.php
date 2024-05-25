f11152852b7081720b5adf8f089e25d37bdb29c5
modollarpo <ariyo.dolapo@gmail.com>
2024-05-25 17:31:16 +0100

    first commit

---
+++ database/seeds/v300/Updatev300Seeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace v300;
+
+use Illuminate\Database\Seeder;
+
+class Updatev300Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
